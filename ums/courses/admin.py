6dfd08f7aed9010afa4eac0cb5bc77e55b2ca410
Khani09 <gtest6979@gmail.com>
2025-09-01 10:19:33 +0500

    just added some code in admin.py file of coures

--- ums/courses/admin.py
+++ ums/courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "program", "batch", "semester", "credits", "course_type", "faculty")
+    list_filter = ("program", "batch", "semester", "course_type")
+    search_fields = ("code", "name")
