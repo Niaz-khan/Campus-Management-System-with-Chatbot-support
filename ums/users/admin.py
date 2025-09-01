c963988c546e299b8f6ced6e3d7bc90fecba53ec
Khani09 <gtest6979@gmail.com>
2025-09-01 09:07:45 +0500

    added models,admin to user app

--- ums/users/admin.py
+++ ums/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """
+    admin for user model
+    """
+    model = User
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff')
+    list_filter = ('role', 'is_active', 'is_staff')
+    ordering = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Roles & Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'role', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
